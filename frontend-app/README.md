05d496c03a6aaa563908e7caf2d7d585136a8012
blockstripe <maryjanyes@gmail.com>
2023-09-24 21:42:53 +0300

    Update trigger_tenant_executable() method, refactor tests

--- frontend-app/README.md
+++
@@ -1,5 +0,0 @@
-### Build and Run
-
-- `yarn`
-- `echo "CONTRACT_NAME=dev-1692537994180-77807393046849" > .env`
-- `yarn start`
--- frontend-app/components/functional/ExecutableList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ExecutableStats = () => {
-    return (
-        <p>Configured payments list</p>
-    );
-};
-
-export default ExecutableStats;
